6d26db0eaef5829437214d69a9a7a1f373be7c5e
Alistair Burrowes <alistair.burrowes@gmail.com>
2021-03-26 08:40:48 +1100

    Finished Part1

--- src/Part1.hs
+++ src/Part1.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE AllowAmbiguousTypes, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE AllowAmbiguousTypes, ScopedTypeVariables, TypeApplications, FlexibleInstances, TypeFamilies, FlexibleContexts #-}
 
 module Part1 where
+
+import Numeric.Natural
 
 class TypeOf a where
   typeOf :: String
@@ -13,3 +15,46 @@ instance TypeOf Char where
 
 instance (TypeOf a, TypeOf b) => TypeOf (a, b) where
   typeOf = "(" ++ typeOf @a ++ ", " ++ typeOf @b ++ ")"
+
+data Z
+data S a
+
+class ReifyNat a where
+  reifyNat :: Natural
+
+instance ReifyNat Z where
+  reifyNat = 0
+
+instance ReifyNat a => ReifyNat (S a) where
+  reifyNat = 1 + reifyNat @a
+
+class IsUnit a where
+  isUnit :: Bool
+
+instance {-# OVERLAPPING #-} IsUnit () where
+  isUnit = True
+
+instance IsUnit a where
+  isUnit = False
+
+guardUnit :: forall a. IsUnit a => a -> Either String a
+guardUnit x = case isUnit @a of
+  True  -> Left "unit is not allowed"
+  False -> Right x
+
+type family Sum a b where
+  Sum Z     b = b
+  Sum (S a) b = S (Sum a b)
+
+type family ElementOf a where
+  ElementOf [[a]] = ElementOf [a]
+  ElementOf [a]   = a
+
+class Flatten a where
+  flatten :: a -> [ElementOf a]
+
+instance (ElementOf [a] ~ a) => Flatten [a] where
+  flatten x = x
+
+instance {-# OVERLAPPING #-} Flatten [a] => Flatten [[a]] where
+  flatten x = flatten (concat x)
